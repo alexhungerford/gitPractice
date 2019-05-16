19e1086d5e7dce3e991e3942a04d3b98ba880a1d
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-05-16 13:51:01 -0700

    practice.c file added

---
+++ practice.c
@@ -0,0 +1,25 @@
+/*
+Practing with git bash and github using C files.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+
+int main(void)
+{
+	int x = 10, y = 2, solution;
+	
+	solution = x * y;
+	printf("Answer: x * y = %d", solution);
+	printf("\n");
+	
+	printf("Answer: x + y = %d", x+y);
+	
+	printf("\n");
+//	system("pause");
+	return 0;
+}
